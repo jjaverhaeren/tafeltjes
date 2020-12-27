fbeb3673ca8dbcf03ee106809da85f92bacc667e
Jørgen Verhaeren <jjaverhaeren@gmail.com>
2020-12-27 14:44:15 +0100

    adds styling

--- src/Components/ChooseTables.js
+++ src/Components/ChooseTables.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./ChooseTables.css"
+import "./ChooseTables.css";
 
 const ChooseTables = props => {
   // console.log(props)
@@ -7,38 +7,43 @@ const ChooseTables = props => {
   return (
     <div className="chooseTables">
       <p>Ik wil graag de tafeltjes van</p>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        1
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        2
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        3
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        4
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        5
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        6
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        7
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        8
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        9
-      </button>
-      <button className="chooseTables_buttons" onClick={props.chooseTable}>
-        10
-      </button>
+      <div className="buttons-container">
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          1
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          2
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          3
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          4
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          5
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          6
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          7
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          8
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          9
+        </button>
+        <button className="chooseTables_buttons" onClick={props.chooseTable}>
+          10
+        </button>
+      </div>
+
       <p>oefenen.</p>
-      <button className="letsGo" onClick={props.renderSums}>Let's Go!</button>
+      <button className="letsGo" onClick={props.renderSums}>
+        Let's Go!
+      </button>
     </div>
   );
 };

d1a22bd65e9c4ee56b8fcee6bea8e1674979f8ba
Jørgen Verhaeren <jjaverhaeren@gmail.com>
2020-12-29 21:56:13 +0100

    ch2s5 styling

--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -1,21 +1,31 @@
 import React from "react";
 import Score from "./Score";
+import Chart from "./Chart";
 import "./ScoreBoard.css";
 
-const ScoreBoard = props => {
+const ScoreBoard = (props) => {
   //   console.log(props.arrayOfSums);
 
   return (
     <div className="scoreBoard">
-      <div className="correct-container">
-        <Score amount={props.rightAnswers} label={"GOED:"} />
-      </div>
-      <div className="wrong-container">
-        <Score amount={props.wrongAnswers} label={"FOUT:"} />
-      </div>
-      <div className="level-container">
-        <h4>Level:</h4>
-        <h3>{props.level}</h3>
+      <div className="correctwrongchart-container">
+        <div className="correctwrong-container">
+          <div className="correct-container">
+            <Score amount={props.rightAnswers} label={"GOED:"} />
+          </div>
+          <div className="wrong-container">
+            <Score amount={props.wrongAnswers} label={"FOUT:"} />
+          </div>
+          <div className="level-container">
+            <h3>{props.level}</h3>
+          </div>
+        </div>
+        <div className="chart-container">
+          <Chart 
+          HP={props.HP}
+          shield={props.shield}
+          />
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/Victory.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Victory.css"
+
+const Victory = props => {
+  //   console.log(props.arrayOfSums);
+
+  return (
+    <div id="victory"className="victory-container_hiden">
+      
+    </div>
+  );
+};
+
+export default Victory;

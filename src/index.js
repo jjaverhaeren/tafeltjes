994e0f33dd7b5e4ec0511575e91da5e1da9535b8
Jørgen Verhaeren <jjaverhaeren@gmail.com>
2020-12-30 23:04:15 +0100

    Adds Fortnite font

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import './fonts/BurbankBigCondensed-Black2'
 
 ReactDOM.render(<App />, document.getElementById("root"));
